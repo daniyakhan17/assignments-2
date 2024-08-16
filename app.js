eed60b35a2eff489330268d9170302e7b25d3b06
daniyakhan17 <daniyakhankhan172005@gmail.com>
2024-08-16 08:43:17 -0700

    assignments

---
+++ app.js
@@ -0,0 +1,26 @@
+// prompt("enter valid password");
+// console.log ("password");
+
+// alert("welcome to js land happy coding!")
+
+
+var username = "daniya";
+console.log ("my name is " + username);
+
+var email = "daniyakhankhan172005@gmail.com";
+console.log (email)
+
+// var age = 18;
+// alert("my age is " + age);
+
+// alert("yuo have visited this side 14 times")
+// var year = 18
+// prompt("enter your birth year");
+// console.log (year)
+
+
+var num1 = 20;
+var num2 = 100;
+var sum = num1 + num2
+console.log ("sum of num1 and num2 is " + sum);
+
